6bb07d212e08c6675f77a0b932eeed8bac1b85b2
ycs1m1yk <satoly4@gmail.com>
2020-01-04 23:49:22 +0900

    합분해

---
+++ src/BOJ/DP/2225.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <limits.h>
+#include <string.h>
+#include <stdlib.h>
+#include <string>
+#include <algorithm>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <utility>
+#include <vector>
+
+/*
+    ❗ 덧셈에 +0을 어느 자리에나 넣을 수 있다.
+
+    1
+    1+1/2
+    1+1+1/1+2/2+1/3
+    1+1+1+1/1+1+2/1+2+1/2+1+1/1+3/3+1/4
+*/
+
+using namespace std;
+const int MAX = 201;
+const int DIVISOR = 1000000000;
+
+long long cache[MAX];
+
+long long d(int N, int K)
+{
+}
+
+int main()
+{
+    int N, K;
+    memset(cache, -1, sizeof(cache));
+    scanf("%d %d", &N, &K);
+}
