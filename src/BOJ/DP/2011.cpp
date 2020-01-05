9d95a65fc699f92d971026ba18c0cc0254de6a21
ycs1m1yk <satoly4@gmail.com>
2020-01-06 08:58:56 +0900

    암호코드 탑다운

--- src/BOJ/DP/2011.cpp
+++ src/BOJ/DP/2011.cpp
@@ -24,25 +24,22 @@ int cache[MAX];
 
 int d(int pos)
 {
-    if (!input[pos])
-        return 1;
     int &ret = cache[pos];
-    if (ret != -1)
+    if (ret)
         return ret;
-    if (input[pos] == '0')
-        return ret = 0;
 
-    ret = d(pos + 1);
-
-    if (input[pos] == '1' && input[pos + 1])
+    if (input[pos] > '0')
+        ret = d(pos - 1);
+    int x = (input[pos - 1] - '0') * 10 + (input[pos] - '0');
+    if (x >= 10 && x <= 26)
     {
-        ret += d(pos + 2);
-        ret %= DIVISOR;
-    }
-    if (input[pos] == '2' && input[pos + 1] && input[pos + 1] <= '6')
-    {
-        ret += d(pos + 2);
-        ret %= DIVISOR;
+        if (pos == 1)
+            ret = 2;
+        else
+        {
+            ret += d(pos - 2);
+            ret %= DIVISOR;
+        }
     }
 
     return ret;
@@ -50,7 +47,14 @@ int d(int pos)
 
 int main()
 {
-    memset(cache, -1, sizeof(cache));
+    //memset(cache, -1, sizeof(cache));
     scanf("%s", input);
-    printf("%d", d(0));
+    if (input[0] == '0')
+    {
+        printf("%d", 0);
+        return 0;
+    }
+    cache[0] = 1;
+    int len = strlen(input);
+    printf("%d", d(len - 1));
 }
