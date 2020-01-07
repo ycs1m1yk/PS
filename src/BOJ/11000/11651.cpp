42c580cb7c224615152a576d816112e1a91ba745
ycs1m1yk <satoly4@gmail.com>
2020-01-07 20:11:56 +0900

    좌표 정렬하기2

---
+++ src/BOJ/11000/11651.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <limits.h>
+#include <string.h>
+#include <stdlib.h>
+#include <string>
+#include <algorithm>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+bool cmp(const pair<int, int> &a, const pair<int, int> &b)
+{
+    if (a.second == b.second)
+        return a.first < b.first;
+
+    return a.second < b.second;
+}
+
+int main()
+{
+    int N, x, y;
+    vector<pair<int, int>> xyVec;
+
+    scanf("%d", &N);
+    while (N--)
+    {
+        scanf("%d %d", &x, &y);
+        xyVec.push_back(make_pair(x, y));
+    }
+    sort(xyVec.begin(), xyVec.end(), cmp);
+    for (auto &p : xyVec)
+    {
+        cout << p.first << " " << p.second << "\n";
+    }
+}
