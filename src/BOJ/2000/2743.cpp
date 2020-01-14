9e32ffecc795828a5ff078f644fe2e856c5c24db
ycs1m1yk <satoly4@gmail.com>
2020-01-14 22:19:54 +0900

    문자열 분석

---
+++ src/BOJ/2000/2743.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <limits.h>
+#include <string.h>
+#include <stdlib.h>
+#include <string>
+#include <algorithm>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <utility>
+#include <vector>
+#include <tuple>
+
+using namespace std;
+const int MAX = 101;
+int main()
+{
+    string word;
+    getline(cin, word);
+    cout << word.size();
+}
