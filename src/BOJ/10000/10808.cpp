47dfefddef4c7b83bfe5380d55afc4ff269c49f8
ycs1m1yk <satoly4@gmail.com>
2020-01-14 21:50:24 +0900

    알파벳 개수

--- src/BOJ/10000/10808.cpp
+++ src/BOJ/10000/10808.cpp
@@ -14,6 +14,24 @@
 #include <tuple>
 
 using namespace std;
+const int MAX = 101;
 int main()
 {
+    int cnt[26] = {0};
+    cnt[25] = 1000;
+    char S[MAX];
+    scanf("%s", S);
+    for (auto letter : S)
+    {
+        if (!letter) // null문자 만나면 break
+            break;
+        cnt[letter - 'a']++;
+    }
+    for (auto pos : cnt)
+    {
+        if (pos >= 1000)
+            printf("%d ", pos - 1000);
+        else
+            printf("%d ", pos);
+    }
 }
