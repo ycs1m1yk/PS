7bbde4d6300a34635b53f51ad04790780ef0112c
ycs1m1yk <satoly4@gmail.com>
2020-05-10 12:37:30 +0900

    30 맞왜틀

--- src/BOJ/10000/10610.cpp
+++ src/BOJ/10000/10610.cpp
@@ -6,22 +6,29 @@ using namespace std;
 int main()
 {
     string N;
+    bool isMutiplesOfThree = false; // 숫자들의 합이 3의 배수이면 true
+    int sum;
     int nArr[10]; // 0~9 숫자개수
+
     memset(nArr, 0, sizeof(nArr));
 
     cin >> N;
     for (auto e : N) // nArr 초기화
     {
         int i = e - '0';
+        sum += i;
         nArr[i]++;
     }
 
-    if (nArr[0] == 0)
+    if (sum % 3 == 0)
+        isMutiplesOfThree = true;
+
+    if (nArr[0] != 0 && isMutiplesOfThree)
     {
-        cout << -1;
+        for (int i = 9; i >= 0; i--)
+            for (int j = nArr[i]; j > 0; j--)
+                cout << i;
     }
     else
-    {
-        // 30의 배수가 되는 가장 큰 수 찾기
-    }
+        cout << -1;
 }
