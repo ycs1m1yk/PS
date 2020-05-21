8fb337b6dbd5d13d26198f51bb8bdc0ed793d570
ycs1m1yk <satoly4@gmail.com>
2020-05-21 10:05:36 +0900

    직사각형으로 나누기

--- src/BOJ/1000/1451.cpp
+++ src/BOJ/1000/1451.cpp
@@ -2,18 +2,18 @@
 
 using namespace std;
 int N, M;
-int total = 0;
-int s1, s2, s3;
+long long total = 0;
+long long s1, s2, s3;
 int rectangle[100][100] = {0};
 int rowTotal[100] = {0};
 int colTotal[100] = {0};
 
-int getSubTotal(int r, int c)
+long long getSubTotal(int r1, int r2, int c1, int c2)
 {
-    int ret = 0;
-    for (int row = 0; row <= r; row++)
+    long long ret = 0;
+    for (int row = r1; row <= r2; row++)
     {
-        for (int col = 0; col <= c; col++)
+        for (int col = c1; col <= c2; col++)
         {
             ret += rectangle[row][col];
         }
@@ -22,18 +22,18 @@ int getSubTotal(int r, int c)
     return ret;
 }
 
-int getRowTotal(int r1, int r2)
+long long getRowTotal(int r1, int r2)
 {
-    int ret = 0;
+    long long ret = 0;
     for (int row = r1; row <= r2; row++)
         ret += rowTotal[row];
 
     return ret;
 }
 
-int getColTotal(int c1, int c2)
+long long getColTotal(int c1, int c2)
 {
-    int ret = 0;
+    long long ret = 0;
     for (int col = c1; col <= c2; col++)
         ret += colTotal[col];
 
@@ -55,67 +55,60 @@ int main()
         total += rowTotal[i];
     }
 
-    int result = 0;
+    long long result = 0;
 
-    s1 = s2 = s3 = 0;
     if (N >= 2 && M >= 2)
     {
+        s1 = 0;
         for (int row = 0; row < N - 1; row++)
         {
-            for (int col = 0; col < M - 1; col++)
+            s1 += rowTotal[row];
+            for (int col = 0; col < M; col++)
             {
-                s1 = getSubTotal(row, col);
-                s2 = getRowTotal(0, row) - s1;
-                s3 = total - s1 - s2;
-                result = max(result, s1 * s2 * s3);
-
-                s2 = getColTotal(0, col) - s1;
-                s3 = total - s1 - s2;
-                result = max(result, s1 * s2 * s3);
-            }
-
-            for (int col = N - 1; col >= 1; col--)
-            {
-                s1 = getSubTotal(row, col);
-                s2 = getRowTotal(0, row) - s1;
-                s3 = total - s1 - s2;
-                result = max(result, s1 * s2 * s3);
-
-                s2 = getColTotal(col, M - 1) - s1;
+                s2 = getSubTotal(row + 1, N - 1, 0, col);
                 s3 = total - s1 - s2;
                 result = max(result, s1 * s2 * s3);
             }
         }
 
+        s1 = 0;
         for (int row = N - 1; row >= 1; row--)
         {
-            for (int col = 0; col < M - 1; col++)
+            s1 += rowTotal[row];
+            for (int col = 0; col < M; col++)
             {
-                s1 = getSubTotal(row, col);
-                s2 = getRowTotal(row, N - 1) - s1;
-                s3 = total - s1 - s2;
-                result = max(result, s1 * s2 * s3);
-
-                s2 = getColTotal(0, col) - s1;
+                s2 = getSubTotal(0, row - 1, 0, col);
                 s3 = total - s1 - s2;
                 result = max(result, s1 * s2 * s3);
             }
+        }
 
-            for (int col = N - 1; col >= 1; col--)
+        s1 = 0;
+        for (int col = 0; col < M - 1; col++)
+        {
+            s1 += colTotal[col];
+            for (int row = 0; row < N; row++)
             {
-                s1 = getSubTotal(row, col);
-                s2 = getRowTotal(row, N - 1) - s1;
+                s2 = getSubTotal(0, row, col + 1, M - 1);
                 s3 = total - s1 - s2;
                 result = max(result, s1 * s2 * s3);
+            }
+        }
 
-                s2 = getColTotal(col, M - 1) - s1;
+        s1 = 0;
+        for (int col = M - 1; col >= 1; col--)
+        {
+            s1 += colTotal[col];
+            for (int row = 0; row < N; row++)
+            {
+                s2 = getSubTotal(0, row, 0, col - 1);
                 s3 = total - s1 - s2;
                 result = max(result, s1 * s2 * s3);
             }
         }
     }
 
-    s1 = s2 = s3 = 0;
+    s1 = 0;
     if (N >= 3)
     {
         for (int row = 0; row < N - 2; row++)
@@ -130,7 +123,7 @@ int main()
         }
     }
 
-    s1 = s2 = s3 = 0;
+    s1 = 0;
     if (M >= 3)
     {
         for (int col = 0; col < M - 2; col++)
