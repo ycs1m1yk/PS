b50eb68aa63e5347066638061cbae94947fc0f94
ycs1m1yk <satoly4@gmail.com>
2020-12-27 21:23:20 +0900

    A+B - 7

---
+++ src/BOJ/2nd/11000/11021.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int T;
+    int A, B;
+    scanf("%d", &T);
+
+    for(int i=1; i<=T; i++)
+    {
+        scanf("%d %d", &A, &B);
+        printf("Case #%d: %d\n", i, A + B);        
+    }
+
+    return 0;
+}
