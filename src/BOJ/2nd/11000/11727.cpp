ea1d098090dd12e06ce91582fbf7efa6675e507f
ycs1m1yk <satoly4@gmail.com>
2021-01-02 21:28:55 +0900

    2xn 타일링 2

---
+++ src/BOJ/2nd/11000/11727.cpp
@@ -0,0 +1,30 @@
+#include <cstdio>
+#include <cstring>
+
+const int MAX = 1001;
+const int DIVISOR = 10007;
+
+int n;
+int cache[MAX];
+
+int solve(int n)
+{
+    if (n == 1)
+        return 1;
+    if (n == 2)
+        return 3;
+
+    int &ret = cache[n];
+    if (ret != -1)
+        return ret;
+
+    return ret = (2 * solve(n - 2) + solve(n - 1)) % DIVISOR;
+}
+
+int main()
+{
+    scanf("%d", &n);
+    memset(cache, -1, sizeof(cache));
+
+    printf("%d", solve(n));
+}
