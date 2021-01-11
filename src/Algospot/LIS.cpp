0f1e786473d2c3e66d0010d3d2bce9951777e2d4
ycs1m1yk <satoly4@gmail.com>
2021-01-11 17:25:11 +0900

    more simple

--- src/Algospot/LIS.cpp
+++ src/Algospot/LIS.cpp
@@ -9,18 +9,18 @@ using namespace std;
 const int MAX = 1000;
 
 int c, n;
-int cache[MAX], S[MAX];
+int cache[MAX + 1], S[MAX];
 
 int lis(int start)
 {
-    int &ret = cache[start];
+    int &ret = cache[start + 1];
     if (ret != -1)
         return ret;
 
     ret = 1;
     for (int next = start + 1; next < n; ++next)
     {
-        if (S[start] < S[next])
+        if (start == -1 || S[start] < S[next])
         {
             ret = max(ret, lis(next) + 1);
         }
@@ -39,11 +39,7 @@ int main()
         memset(cache, -1, sizeof(cache));
         for (int i = 0; i < n; ++i)
             scanf("%d", &S[i]);
-
-        int answer = 0;
-        for (int begin = 0; begin < n; ++begin)
-            answer = max(answer, lis(begin));
-
-        printf("%d\n", answer);
+        // S[-1] = -∞ 라고 가정하고 S[0]부터 순회하도록
+        printf("%d\n", lis(-1) - 1);
     }
 }
