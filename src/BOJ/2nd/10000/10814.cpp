7f9308e7b83e9f637356b6bfa237af65f7870f2b
ycs1m1yk <satoly4@gmail.com>
2021-03-18 16:23:04 +0900

    나이순 정렬

---
+++ src/BOJ/2nd/10000/10814.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+const int MAX = 100000;
+
+int N;
+vector<pair<pair<int, int>, string>> users;
+
+int main()
+{
+    fastio;
+    cin >> N;
+    int age;
+    string name;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> age >> name;
+        pair<pair<int, int>, string> user = make_pair(make_pair(age, i), name);
+        users.push_back(user);
+    }
+
+    sort(users.begin(), users.end(), [](const auto &left, const auto &right) {
+        if (left.first.first == right.first.first)
+            return left.first.second < right.first.second;
+        else
+            return left.first.first < right.first.first;
+    });
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << users[i].first.first << " " << users[i].second << "\n";
+    }
+}
