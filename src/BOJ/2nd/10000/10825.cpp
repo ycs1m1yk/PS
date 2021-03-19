59a9cc404cf5ccd2a44fe99273af8ec33e07ec51
ycs1m1yk <satoly4@gmail.com>
2021-03-19 21:32:19 +0900

    국영수 개선

--- src/BOJ/2nd/10000/10825.cpp
+++ src/BOJ/2nd/10000/10825.cpp
@@ -4,7 +4,7 @@
 #define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 using namespace std;
 const int MAX = 100000;
-typedef tuple<string, int, int, int> score;
+typedef tuple<int, int, int, string> score;
 
 int N;
 score s[MAX];
@@ -18,18 +18,13 @@ int main()
         string name;
         int kor, eng, math;
         cin >> name >> kor >> eng >> math;
-        // 영어 점수는 증가순
-        s[i] = score(name, kor, -1 * eng, math);
+        // 국어와 수학 점수는 감소순, 이름은 마지막에 비교
+        s[i] = score(-kor, eng, -math, name);
     }
 
-    sort(s, s + N, [](const auto &left, const auto &right) {
-        // 이름을 int형으로 비교한다.
-        int rc = get<0>(left).compare(get<0>(right));
-        int zero = 0;
-        // 이름이 증가순이므로 left에 zero, right에 rc값을 비교
-        return tie(get<1>(left), get<2>(left), get<3>(left), zero) > tie(get<1>(right), get<2>(right), get<3>(right), rc);
-    });
+    // sort using the default operator<
+    sort(s, s + N);
 
     for (int i = 0; i < N; i++)
-        cout << get<0>(s[i]) << "\n";
+        cout << get<3>(s[i]) << "\n";
 }
