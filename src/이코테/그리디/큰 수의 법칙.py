b316113ccd711017e92301b5896b654ea5622a3c
ycs1m1yk <satoly4@gmail.com>
2021-07-23 11:37:44 +0900

    큰 수의 법칙 수정

--- src/이코테/그리디/큰 수의 법칙.py
+++ src/이코테/그리디/큰 수의 법칙.py
@@ -4,16 +4,9 @@ data = list(map(int, input().split()))
 data.sort(reverse=True)
 
 first = data[0]
-second = 0
-first_cnt = 0
-for i in range(len(data)):
-    if data[i] == first:
-        first_cnt += 1
-    else:
-        second = data[i]
-        break
+second = data[1]
 
-cycle = k*first_cnt+1
+cycle = k+1
 cycle_sum = (cycle-1)*first+second
 answer = (m//cycle)*cycle_sum + (m % cycle)*first
 print(answer)
