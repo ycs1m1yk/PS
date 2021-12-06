97c542cfac1c002fa37212a1fa6cea81501fb29b
Hyeseong Lee <satoly4@gmail.com>
2021-12-06 21:42:49 +0900

    프린터 큐 리팩토링

--- src/BOJ/1000/1966.cpp
+++ src/BOJ/1000/1966.cpp
@@ -6,41 +6,37 @@
 using namespace std;
 typedef pair<int, int> pii;
 
-int main(){
+int main()
+{
     int T, N, M;
-
     cin >> T;
-    while (T--) {
+    while (T--)
+    {
         cin >> N >> M;
-        queue<pii> task, done;
-        priority_queue<int, vector<int>, less<int> > sorted;
-        int priority, minPriority=10;
-        for(int i = 0; i < N; i++){
+        queue<pii> task;
+        priority_queue<int> pq;
+        int priority;
+        for (int i = 0; i < N; i++)
+        {
             cin >> priority;
-            minPriority = min(minPriority, priority);
-            task.push({priority, i});           
-            sorted.push(priority);           
+            task.push({priority, i});
+            pq.push(priority);
         }
 
-        while(!task.empty()){
-            int now = sorted.top();
-            if(now!=minPriority){
-                while(task.front().first < now){
-                    task.push(task.front());
-                    task.pop();
-                }
+        for (int ans = 1;; ans++)
+        {
+            while (task.front().first < pq.top())
+            {
+                task.push(task.front());
+                task.pop();
             }
-            done.push(task.front());
-            task.pop();
-            sorted.pop();
-        }
-        int ans=0;
-        while(!done.empty()){
-            ans++;
-            if(done.front().second == M)
+            if (task.front().second == M)
+            {
+                cout << ans << "\n";
                 break;
-            done.pop();
+            }
+            task.pop();
+            pq.pop();
         }
-        cout<<ans<< "\n";
     }
 }
