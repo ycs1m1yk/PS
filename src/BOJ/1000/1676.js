408e35e23efd0f154ad73187eb85d08d36e20134
Hyeseong Lee <satoly4@gmail.com>
2022-03-11 22:05:54 +0900

    팩토리얼 0의 개수 개선

--- src/BOJ/1000/1676.js
+++ src/BOJ/1000/1676.js
@@ -1,22 +1,8 @@
 const filePath = process.platform === "linux" ? "/dev/stdin" : "test.txt";
 const N = Number(require("fs").readFileSync(filePath).toString().trim());
 
-if (N === 0) {
-  console.log(0);
-  process.exit();
+let fiveCount = 0;
+for (let i = 5; i <= N; i *= 5) {
+  fiveCount += parseInt(N / i);
 }
-
-let twoCount = 0,
-  fiveCount = 0;
-for (let i = 1; i <= N; i++) {
-  let iCopy = i;
-  while (iCopy % 2 === 0) {
-    iCopy /= 2;
-    twoCount++;
-  }
-  while (iCopy % 5 === 0) {
-    iCopy /= 5;
-    fiveCount++;
-  }
-}
-console.log(Math.min(twoCount, fiveCount));
+console.log(fiveCount);
