ab42fc9ea1bb7c3ce9890346949e2a23446b7b71
Hyeseong Lee <satoly4@gmail.com>
2022-04-07 16:57:34 +0900

    회의실 배정

--- src/BOJ/1000/1931.js
+++ src/BOJ/1000/1931.js
@@ -10,10 +10,9 @@ meetings.sort((a, b) => {
   return a[1] === b[1] ? a[0] - b[0] : a[1] - b[1];
 });
 
-console.log(meetings);
-var newBegin = -1;
-var newEnd = -1;
-var ans = 0;
+let newBegin = -1;
+let newEnd = -1;
+let ans = 0;
 for (const [begin, end] of meetings) {
   if (newEnd <= begin) {
     newBegin = begin;
